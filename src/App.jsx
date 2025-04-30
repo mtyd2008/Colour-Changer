90549333b008c1c920c10293e109e3fcc622e803
mtyd2008 <tahaydandya@gmail.com>
2025-04-30 18:42:37 +0500

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,13 @@ const App = () => {
     <>
     <div style={{
       display:'flex',
-      justifyContent:'center',
+      justifyContent:'space-between',
       alignItems:'center',
+      marginLeft:'50px'
     }}>
     <h1>Color Changer</h1>
 
-    </div>
+    
 
     <div style={{
       display:'flex',
@@ -31,21 +32,29 @@ const App = () => {
       alignItems:'center',
       gap:'10px',
       cursor:'pointer',
-      border:'none',
-      borderRadius:'20px',
+      marginRight:'50px'
     }}>
-    <button onClick={()=>Changer('white')}>Default</button>
-    <button onClick={()=>Changer('yellow')}>Yellow</button>
-    <button onClick={()=>Changer('blue')}>Blue</button>
-    <button onClick={()=>Changer('red')}>Red</button>
-    <button onClick={()=>Changer('pink')}>Pink</button>
-    <button onClick={()=>Changer('green')}>Green</button>
-    <button onClick={()=>Changer('purple')}>Purple</button>
-    <button onClick={()=>Changer('gray')}>Gray</button>
-
+    <button style={{backgroundColor:'white' , padding:'8px' , borderRadius:'8px', border:'1px solid black'}}
+     onClick={()=>Changer('white')}>Default</button>
+    <button style={{backgroundColor:'yellow' , padding:'8px' , borderRadius:'8px', border:'1px solid yellow'}}
+     onClick={()=>Changer('yellow')}>Yellow</button>
+    <button style={{backgroundColor:'blue' , padding:'8px' , borderRadius:'8px', border:'1px solid blue'}}
+     onClick={()=>Changer('blue')}>Blue</button>
+    <button style={{backgroundColor:'red' , padding:'8px' , borderRadius:'8px', border:'1px solid red'}}
+     onClick={()=>Changer('red')}>Red</button>
+    <button style={{backgroundColor:'pink' , padding:'8px' , borderRadius:'8px', border:'1px solid pink'}}
+     onClick={()=>Changer('pink')}>Pink</button>
+    <button style={{backgroundColor:'green' , padding:'8px' , borderRadius:'8px', border:'1px solid green'}}
+     onClick={()=>Changer('green')}>Green</button>
+    <button style={{backgroundColor:'purple' , padding:'8px' , borderRadius:'8px', border:'1px solid purple'}}
+     onClick={()=>Changer('purple')}>Purple</button>
+    <button style={{backgroundColor:'gray', padding:'8px' , borderRadius:'8px', border:'1px solid gray'}}
+     onClick={()=>Changer('gray')}>Gray</button>
+     <button style={{backgroundColor:'orange', padding:'8px' , borderRadius:'8px', border:'1px solid orange'}}
+     onClick={()=>Changer('orange')}>Orange</button>
     </div>
     
-
+    </div>
     </>
   )
 }
